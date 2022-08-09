8739011f46e2eef278efc636fc663e042c798957
Anastasia <nastya-orsk83@mail.ru>
2022-08-09 15:18:38 +0300

    Homework1

---
+++ Homework 1/Program.cs
@@ -0,0 +1,50 @@
+﻿/*int a = 5;
+int b = 7;
+
+int max = a;
+if (b > max) max = b;
+
+Console.WriteLine(max);*/
+
+/*int a = 2;
+int b = 10;
+
+int max = a;
+if (b > max) max = b;
+
+Console.WriteLine(max);*/
+
+/*int a = -9;
+int b = -3;
+
+int max = a;
+if (b > max) max = b;
+
+Console.WriteLine(max);*/
+
+/*int n1 = 2;
+int n2 = 3;
+int n3 = 7;
+int max = n1;
+    if(n2 > max ) max = n2;
+    if(n3 > max ) max = n3;
+
+Console.WriteLine(max);*/
+
+/*int n1 = 44;
+int n2 = 5;
+int n3 = 78;
+int max = n1;
+    if(n2 > max ) max = n2;
+    if(n3 > max ) max = n3;
+
+Console.WriteLine(max);*/
+
+/*int n1 = 22;
+int n2 = 3;
+int n3 = 9;
+int max = n1;
+    if (n2 > max) max = n2;
+    if (n3 > max) max = n3;
+
+Console.WriteLine(max);*/

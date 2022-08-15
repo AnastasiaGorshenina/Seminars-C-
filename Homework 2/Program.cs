adec01b910aa6f9c6c47e7b4402da3447d666b87
Anastasia <nastya-orsk83@mail.ru>
2022-08-15 17:26:58 +0300

    Homework2

---
+++ Homework 2/Program.cs
@@ -0,0 +1,40 @@
+﻿//Задача 1
+/*
+int ThreeSotDigit (int number)
+{
+   int current = (number / 10)%10;
+   return current; 
+}
+
+int randomNumber = new Random().Next(100,1000);    
+int NewCutnumber=ThreeSotDigit(randomNumber);
+
+Console.WriteLine($"New number of {randomNumber} is {NewCutnumber}");
+*/
+
+
+//Задача 3.
+/*
+void WeekEnds (int number)
+{
+    
+    if( number == 6 || number == 7)
+    {
+        Console.WriteLine("выходной");
+    } 
+    else
+    {
+        Console.WriteLine("Будни");
+    }
+       
+}
+Console.Write("Input  number:");
+int n =Convert.ToInt32(Console.ReadLine());
+WeekEnds(n);
+ */  
+
+
+//Задача 2.
+
+
+

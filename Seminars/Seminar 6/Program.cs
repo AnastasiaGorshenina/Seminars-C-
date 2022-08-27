25f1696f35b8e6f5cedfa60c7a80b09e408cf784
Anastasia <nastya-orsk83@mail.ru>
2022-08-27 13:28:09 +0300

    added changes Semina6

--- Seminars/Seminar 6/Program.cs
+++ Seminars/Seminar 6/Program.cs
@@ -1,117 +1,15 @@
-﻿*
-// Задача 1. Задайте массив из 12 элементов, заполненный случайными числами из промежутка [-9, 9]. Найдите сумму отрицательных и положительных элементов массива.
-int[] CreateRandomArray(int size, int minValue, int maxValue)
+﻿/*
+// Задача 1. Напишите программу, которая перевернёт одномерный массив (последний элемент будет на первом месте, а первый - на последнем и т.д.)
+int [] ReverseArray(int[] array)
 {
-int [] newArray = new int[size];
-for (int i = 0; i < size; i++)
-    newArray[i] = new Random().Next(minValue, maxValue + 1);
-    return newArray;
-}
-void ShowArray(int[] array)
-{
-    for(int i = 0; i < array.Length; i++)
-        Console.Write(array[i] + " ");
-    Console.WriteLine();
-}
-void FindMinMaxSum(int[] array)
-{
-    int sumPositive = 0;
-    int sumNegative = 0;
-    for (int i = 0; i < array.Length; i++)
+    for(int i = 0, j= array.Length - 1; i < j; i++, j--)
     {
-        if (array[i] > 0)
-            sumPositive += array[i];
-        else
-            sumNegative += array[i];
-    }
-    Console.WriteLine("Sum of negatives elements is " + sumNegative);
-    Console.WriteLine("Sum of positives elements is " + sumPositive);
-}
-Console.Write("Input size for array: ");
-int a = Convert.ToInt32(Console.ReadLine());
-Console.Write("Input min possible value of element: ");
-int min = Convert.ToInt32(Console.ReadLine());
-Console.Write("Input max possible value of element: ");
-int max = Convert.ToInt32(Console.ReadLine());
-int[] myArray = CreateRandomArray (a, min, max);
-ShowArray(myArray);
-FindMinMaxSum(myArray);
-*/
-
-/*
-// Задача 2. Напишите программу замены элементов массива: положительные элементы замените на соответствующие отрицательные, и наоборот.
-int[] CreateRandomArray(int size, int minValue, int maxValue)
-{
-int [] newArray = new int[size];
-for (int i = 0; i < size; i++)
-    newArray[i] = new Random().Next(minValue, maxValue + 1);
-    return newArray;
-}
-void ShowArray(int[] array)
-{
-    for(int i = 0; i < array.Length; i++)
-        Console.Write(array[i] + " ");
-    Console.WriteLine();
-}
-int[] ChangeElements(int[] array)
-{
-    for (int i = 0; i < array.Length; i++)
-    {
-       array[i] = array[i] * (-1);
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
     }
     return array;
 }
-Console.Write("Input size for array: ");
-int a = Convert.ToInt32(Console.ReadLine());
-Console.Write("Input min possible value of element: ");
-int min = Convert.ToInt32(Console.ReadLine());
-Console.Write("Input max possible value of element: ");
-int max = Convert.ToInt32(Console.ReadLine());
-int[] myArray = CreateRandomArray (a, min, max);
-ShowArray(myArray);
-ShowArray(ChangeElements(myArray));
-*/
-
-/*
-// Задача 3. Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве.
-int[] CreateArray(int size)
-{
-    int [] newArray = new int[size];
-    for (int i = 0; i < size; i++)
-        {
-        Console.Write($"Input {i} element of the array: ");
-        newArray[i] = Convert.ToInt32(Console.ReadLine());    
-        }
-    return newArray;
-}
-void ShowArray(int[] array)
-{
-    for(int i = 0; i < array.Length; i++)
-        Console.Write(array[i] + " ");
-    Console.WriteLine();
-}
-bool CheckNumber(int[] array, int n)
-{
-    for (int i = 0; i < array.Length; i++)
-    {
-        if (array[i] == n) return true; // return if (array[i] == n) 
-    }
-    return false;
-}
-Console.Write("Input size for array: ");
-int a = Convert.ToInt32(Console.ReadLine());
-Console.Write("Input integer number: ");
-int number = Convert.ToInt32(Console.ReadLine());
-int[] myArray = CreateArray (a);
-ShowArray(myArray);
-bool check = CheckNumber(myArray, number);
-if (check == true) Console.WriteLine($"Число {number} найдено в заданном массиве");
-else Console.WriteLine($"Число {number} не найдено в заданном массиве");
-*/
- 
-
-/*
-// Задача 4. Задайте одномерный массив из 12 случайных чисел. Найдите количество элементов массива, значения которых лежат в отрезке [10,99].
 int[] CreateRandomArray(int size, int minValue, int maxValue)
 {
 int [] newArray = new int[size];
@@ -125,41 +23,48 @@ void ShowArray(int[] array)
         Console.Write(array[i] + " ");
     Console.WriteLine();
 }
-int CountElements(int[] array, int min, int max)
-{
-    int count = 0;
-    
-    for (int i = 0; i < array.Length; i++)
-    {
-        if (array[i] >= min && array[i]<= max) count++;
-    }
-   return count;
-}
 Console.Write("Input size for array: ");
-int a = Convert.ToInt32(Console.ReadLine());
+int size = Convert.ToInt32(Console.ReadLine());
 Console.Write("Input min possible value of element: ");
 int min = Convert.ToInt32(Console.ReadLine());
 Console.Write("Input max possible value of element: ");
 int max = Convert.ToInt32(Console.ReadLine());
-Console.Write("Input min value: ");
-int min1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Input max value: ");
-int max1 = Convert.ToInt32(Console.ReadLine());
-int[] myArray = CreateRandomArray (a, min, max);
+int[] myArray = CreateRandomArray (size, min, max);
 ShowArray(myArray);
-int result = CountElements(myArray, min1, max1);
-Console.WriteLine($"Количество элементов массива, находящихся в заданном отрезке, равно {result}");
+ShowArray(ReverseArray(myArray));
 */
 
+/*
+// Задача 2. Напишите программу, которая принимает на вход три числа и проверяет, может ли существовать треугольник с сторонами такой длины.
+// a < b + c
+bool CheckTriangle(int a, int b, int c)
+{
+    if ((a+b>c) && (a+c>b) && (b+c>a)) return true;
+    else return false;
+}
+Console.Write("Input fist length: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input second length: ");
+int y = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input third length: ");
+int z = Convert.ToInt32(Console.ReadLine());
+bool result = CheckTriangle(x, y, z);
+if (result == true) Console.WriteLine($"Triangle with sides: {x}, {y}, {z} exists");
+else Console.WriteLine($"Triangle with sides: {x}, {y}, {z} does not exist");
+*/
 
 /*
-// Найдите произведение пар чисел в одномерном массиве. Парой считаем первый и последний элемент, второй и предпоследний и т.д. Результат запишите в новом массиве.
-int[] CreateRandomArray(int size, int minValue, int maxValue)
+// Задача 3. Не используя рекурсию, выведите первые N чисел Фибоначчи. Первые два числа Фибоначчи: a и b.
+int[] Fibbonachi(int firstNumber, int secondNumber, int size)
 {
-int [] newArray = new int[size];
-for (int i = 0; i < size; i++)
-    newArray[i] = new Random().Next(minValue, maxValue + 1);
-    return newArray;
+    int[] array = new int [size];
+    array[0] = firstNumber;
+    array[1] = secondNumber;
+    for(int i = 2; i < size; i++)
+    {
+        array[i] = array[i-2] + array[i - 1];
+    }
+    return array;
 }
 void ShowArray(int[] array)
 {
@@ -167,22 +72,31 @@ void ShowArray(int[] array)
         Console.Write(array[i] + " ");
     Console.WriteLine();
 }
-int[] CompositionPairsOfElements(int[] array)
-{
-    int[] newArray = new int [array.Length / 2];
-    for (int i = 0; i < array.Length / 2; i++)
-    {
-        newArray[i] = array[i]*array[array.Length - 1 - i];
-    }
-   return newArray;
-}
-Console.Write("Input size for array: ");
-int a = Convert.ToInt32(Console.ReadLine());
-Console.Write("Input min possible value of element: ");
-int min = Convert.ToInt32(Console.ReadLine());
-Console.Write("Input max possible value of element: ");
-int max = Convert.ToInt32(Console.ReadLine());
-int[] myArray = CreateRandomArray (a, min, max);
-ShowArray(myArray);
-ShowArray(CompositionPairsOfElements(myArray));
+Console.Write("Input fist number: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input second number: ");
+int y = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input array size: ");
+int z = Convert.ToInt32(Console.ReadLine());
+ShowArray(Fibbonachi(x, y, z));
 */
+
+// Задача 4. Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+
+void DecimalToBinary (int number)
+{
+    string result = string.Empty;
+    while (number > 0)
+    {
+        result = number % 2 + result;
+        number /= 2; 
+    }
+    Console.WriteLine(result);
+}
+
+Console.Write("Input decimal number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+
+Console.Write($"Number {num} to binaty is ");
+DecimalToBinary(num);

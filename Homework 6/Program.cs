1e1ef127d5a95e0c93fd633e63055e13ae31be9c
Anastasia <nastya-orsk83@mail.ru>
2022-08-30 00:34:33 +0300

    added changes

--- Homework 6/Program.cs
+++ Homework 6/Program.cs
@@ -47,7 +47,7 @@ FindColNumbers(myArray);
 /*
 void FindCrossPoint (double k1, double k2, double b1, double b2)
 {
-double x = (-b2 + b1)/(-k1 + k2);
+double x = (b1 - b2)/(k2 - k1);
 double y = k2 * x + b2;
 
 Console.Write($"Точка пересечения заданных прямых: ({x}; {y})");
